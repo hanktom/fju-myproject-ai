3eb0a623af0c81be9a206378aa458bc1e580761c
Hank Tom <hanktom@gmail.com>
2021-10-19 11:38:21 +0800

    initial commit

---
+++ src/com/tom/Hello.java
@@ -0,0 +1,15 @@
+package com.tom;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		int x = 17;
+		x = x * 3;
+		System.out.print(x);
+		System.out.print("Hello " + x);
+		double d = Math.random();
+		System.out.println();
+		System.out.println(Math.ceil(4.8));
+	}
+
+}
